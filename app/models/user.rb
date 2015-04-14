484ed31a7444959585cda0cc47ae64efdf4186ec
Marcin Słowik <riobson@gmail.com>
2015-04-14 12:39:25 +0000

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  
   def full_name
     [first_name, last_name].join(' ')
   end
